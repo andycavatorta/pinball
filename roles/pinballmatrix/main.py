a9fefac720338d51d6b616b2b533d6299c20b613
Andy Cavatorta <hellyeah@media.mit.edu>
2021-03-08 18:54:48 -0500

    starting homing routines

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -98,7 +98,7 @@ class Main(threading.Thread):
 
     def home(self):
         for motor_name in self.motor_names:
-            self.controllers.motors[motor_name].go_to_absolute_position(0)
+            self.controllers.motors[motor_name].go_to_absolute_position(1000)
         # are all controllers are responding?
 
         # are all abs encoders responding?
