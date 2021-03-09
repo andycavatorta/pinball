45526d2e97e2a540c9e287e3373e662dea5061e2
Andy Cavatorta <hellyeah@media.mit.edu>
2021-03-08 19:12:41 -0500

    starting homing routines

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -98,10 +98,10 @@ class Main(threading.Thread):
 
     def home(self):
         for motor_name in self.motor_names:
-            self.controllers.motors[motor_name].go_to_absolute_position(0)
+            self.controllers.motors[motor_name].go_to_speed_or_relative_position(0)
         time.sleep(3)
         for motor_name in self.motor_names:
-            self.controllers.motors[motor_name].go_to_absolute_position(4096)
+            self.controllers.motors[motor_name].go_to_speed_or_relative_position(4096)
         time.sleep(3)
         # are all controllers are responding?
 
