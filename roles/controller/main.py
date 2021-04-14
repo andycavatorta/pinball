5413a0d4b64425eccceff3bf058f96f41b3c18de
Andy Cavatorta <hellyeah@media.mit.edu>
2021-04-14 18:30:14 -0400

    enable

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -41,7 +41,7 @@ class Safety_Enable(threading.Thread):
             except queue.Empty:
                 pass
             print(all(elem in self.required_hosts  for elem in self.hosts_alive), self.required_hosts, self.hosts_alive)
-            GPIO.output(setting_safety_enable_gpio, GPIO.HIGH if all(elem in self.hosts_alive for elem in self.required_hosts) else GPIO.LOW)
+            GPIO.output(setting_safety_enable_gpio, GPIO.HIGH if all(elem in self.required_hosts  for elem in self.hosts_alive) else GPIO.LOW)
             self.hosts_alive = []
             time.sleep(2)
             
