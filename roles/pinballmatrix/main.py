8d169aa860c8366cf1500d4d6321b7dd15e03511
Andy Cavatorta <hellyeah@media.mit.edu>
2021-05-02 23:26:14 -0400

    working on global game modes

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -5,6 +5,7 @@ import queue
 import sys
 import threading
 import time
+import traceback
 
 app_path = os.path.dirname((os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 sys.path.append(os.path.split(app_path)[0])
@@ -72,9 +73,11 @@ class Main(threading.Thread):
             self.network_status_change_handler,
             self.exception_handler
         )
+        self.game_modes = settings.Game_Modes()
+        self.game_mode = self.game_modes.WAITING_FOR_CONNECTIONS
         self.safety_enable = Safety_Enable(self.tb)
+        self.tb.subscribe_to_topic("set_game_mode")
         self.tb.subscribe_to_topic("connected")
-        self.tb.subscribe_to_topic("home")
 
         #self.create_controllers_and_motors()
         #time.sleep(3)
@@ -145,53 +148,54 @@ class Main(threading.Thread):
 
     def status_receiver(self, msg):
         print("status_receiver", msg)
-    def network_message_handler(self, topic, message):
-        self.add_to_queue(topic, message)
+    def network_message_handler(self, topic, message, origin, destination):
+        self.add_to_queue(topic, message, origin, destination)
     def exception_handler(self, exception):
         print("exception_handler",exception)
     def network_status_change_handler(self, status, hostname):
         print("network_status_change_handler", status, hostname)
+    def add_to_queue(self, topic, message, origin, destination):
+        self.queue.put((topic, message, origin, destination))
+    def set_game_mode(self, mode):
+        if mode == self.game_modes.WAITING_FOR_CONNECTIONS:
+            self.game_mode = self.game_modes.WAITING_FOR_CONNECTIONS
+            pass # peripheral power should be off during this mode
+        if mode == self.game_modes.RESET:
+            self.game_mode = self.game_modes.RESET
+            time.sleep(6)
+            self.tb.publish("ready_state",True)
 
-    def add_to_queue(self, topic, message):
-        self.queue.put((topic, message))
+        if mode == self.game_modes.ATTRACTION:
+            self.game_mode = self.game_modes.ATTRACTION
+        # waits for press of start button 
+        """
+        if mode == self.game_modes.COUNTDOWN:
+            #self.player.play("countdown_mode")
+        if mode == self.game_modes.BARTER_MODE_INTRO:
+            self.player.play("barter_mode_intro")
+        if mode == self.game_modes.BARTER_MODE:
+            self.player.play("barter_mode")
+        if mode == self.game_modes.MONEY_MODE_INTRO:
+            self.player.play("money_mode_intro")
+        if mode == self.game_modes.MONEY_MODE:
+            self.player.play("money_mode")
+        if mode == self.game_modes.ENDING:
+            self.player.play("closing_theme")
+        if mode == self.game_modes.RESET:
+            self.player.play("reset")
+        if mode == self.game_modes.ERROR:
+            pass
+        """
+
     def run(self):
         while True:
-            """
-            v.motors["carousel_1"].go_to_speed_or_relative_position(0)
-            time.sleep(1)
-            self.controllers.motors["carousel_2"].go_to_speed_or_relative_position(0)
-            time.sleep(1)
-            self.controllers.motors["carousel_3"].go_to_speed_or_relative_position(0)
-            time.sleep(1)
-            self.controllers.motors["carousel_4"].go_to_speed_or_relative_position(0)
-            time.sleep(1)
-            self.controllers.motors["carousel_5"].go_to_speed_or_relative_position(0)
-            time.sleep(1)
-            self.controllers.motors["carousel_6"].go_to_speed_or_relative_position(0)
-            time.sleep(3)
-
-            self.controllers.motors["carousel_1"].go_to_speed_or_relative_position(2048)
-            time.sleep(1)
-            self.controllers.motors["carousel_2"].go_to_speed_or_relative_position(2048)
-            time.sleep(1)
-            self.controllers.motors["carousel_3"].go_to_speed_or_relative_position(2048)
-            time.sleep(1)
-            self.controllers.motors["carousel_4"].go_to_speed_or_relative_position(2048)
-            time.sleep(1)
-            self.controllers.motors["carousel_5"].go_to_speed_or_relative_position(2048)
-            time.sleep(1)
-            self.controllers.motors["carousel_6"].go_to_speed_or_relative_position(2048)
-            """
-            time.sleep(3)
-            #self.home()
-            """
             try:
-                topic, message = self.queue.get(True)
-                print(">>>",topic, message)
-
+                topic, message, origin, destination = self.queue.get(True)
+                print(topic, message)
+                if topic == b'set_game_mode':
+                    self.set_game_mode(message)
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
-            """
 main = Main()
 
