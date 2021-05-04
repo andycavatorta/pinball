e993cd2a5961a899e0dcdbd3b813d0fb868092e8
Matt Ross <mattross1080@gmail.com>
2021-05-04 19:17:46 -0400

    fixes extra var

--- roles/pinball4game/main.py
+++ roles/pinball4game/main.py
@@ -32,7 +32,6 @@ class MPF_Bridge(threading.Thread):
             try:
               message = self.socket.recv()
               print(f"Received msg#: {message}")
-              count = count +1
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
