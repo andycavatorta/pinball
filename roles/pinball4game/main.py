5bccc46b2276a678cc344a6e10aea6d9c6dd9a7c
Matt Ross <mattross1080@gmail.com>
2021-05-05 11:30:09 -0400

    adds str for msg

--- roles/pinball4game/main.py
+++ roles/pinball4game/main.py
@@ -32,7 +32,7 @@ class MPF_Bridge(threading.Thread):
             try:
               message = self.socket.recv()
               print(f"Received msg#: {message}")
-              self.tb.publish("gameupdate", message)
+              self.tb.publish("gameupdate", str(message))
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
