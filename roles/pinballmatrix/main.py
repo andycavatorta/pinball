2c7662506ded016ce053728001a02c0676b5ecfa
Andy Cavatorta <hellyeah@media.mit.edu>
2021-07-11 19:39:00 -0500

    starting refactor of pinballmatrix

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -16,6 +16,12 @@ from thirtybirds3.adapters.actuators import roboteq_command_wrapper
 from thirtybirds3.adapters.sensors.AMT203_encoder import AMT203_absolute_encoder
 
 
+##################################################
+# SAFETY SYSTEMS #
+##################################################
+
+# DEADMAN SWITCH ( SEND ANY OUT-OF-BOUNDS VALUES FROM OTHER SYSTEMS)
+
 class Safety_Enable(threading.Thread):
     def __init__(self, tb):
         threading.Thread.__init__(self)
@@ -32,6 +38,38 @@ class Safety_Enable(threading.Thread):
             self.tb.publish("deadman", "safe")
             time.sleep(1)
 
+# COMPUTER STATUS CHECK
+    # runs on slower rhythm than deadman
+    # only log report if there is a problem
+
+# 48V CURRENT METER current_sensor_handler
+
+##################################################
+# LOGGING AND REPORTING #
+##################################################
+
+# EXCEPTION
+
+# STATUS MESSAGES
+
+# LOCAL LOGGING / ROTATION
+
+##################################################
+# MAIN, TB, STATES, AND TOPICS #
+##################################################
+
+
+##########
+# STATES #
+##########
+
+###################
+# HARDWARE SETUP  #
+###################
+
+# set up motor controllers
+# set up references to motors
+# set up proxies for carousels?
 
 class Roboteq_Data_Receiver(threading.Thread):
     def __init__(self):
@@ -49,6 +87,432 @@ class Roboteq_Data_Receiver(threading.Thread):
             #if "internal_event" in message:
             #    pass
 roboteq_data_receiver = Roboteq_Data_Receiver()
+
+class Motor_Controllers(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.controller_names = ["carousel1and2", "carousel3and4","carousel5and6"]
+        self.motor_names = ["carousel_1","carousel_2","carousel_3","carousel_4","carousel_5","carousel_6"]
+        self.chip_select_pins_for_abs_enc = [8,7,18,17,16,5]
+        self.ppr = 4096
+        self.queue = queue.Queue()
+        self.create_controllers_and_motors()
+        time.sleep(1)
+        #self.set_rel_encoder_position_to_abs_encoder_position()
+        #self.home()
+        self.start()
+
+    def create_controllers_and_motors(self):
+        self.controllers = roboteq_command_wrapper.Controllers(
+            roboteq_data_receiver.add_to_queue, 
+            self.status_receiver, 
+            self.network_status_change_handler, 
+            {
+                "carousel1and2":settings.Roboteq.BOARDS["carousel1and2"],
+                "carousel3and4":settings.Roboteq.BOARDS["carousel3and4"],
+                "carousel5and6":settings.Roboteq.BOARDS["carousel5and6"],
+            },
+            {
+                "carousel_1":settings.Roboteq.MOTORS["carousel_1"],
+                "carousel_2":settings.Roboteq.MOTORS["carousel_2"],
+                "carousel_3":settings.Roboteq.MOTORS["carousel_3"],
+                "carousel_4":settings.Roboteq.MOTORS["carousel_4"],
+                "carousel_5":settings.Roboteq.MOTORS["carousel_5"],
+                "carousel_6":settings.Roboteq.MOTORS["carousel_6"],
+            }
+        )
+        for motor_name_ordinal in enumerate(self.motor_names):
+            self.controllers.motors[motor_name_ordinal[1]].absolute_encoder = AMT203_absolute_encoder.AMT203(cs=self.chip_select_pins_for_abs_enc[motor_name_ordinal[0]])
+
+    """
+    def set_rel_encoder_position_to_abs_encoder_position(self):
+        for motor_name in self.motor_names:
+            abs_pos = self.controllers.motors[motor_name].absolute_encoder.get_position()
+            self.controllers.motors[motor_name].set_encoder_counter(abs_pos)
+
+    def ppr_to_mils(self, ppr):
+        return int(((ppr-2048)/2048*1000))
+
+    def mils_to_ppr(self, mils):
+        return int(((float(mils)/1000.0)*2048.0)+2048.0)
+
+    def home(self):
+        for motor_name in self.motor_names:
+            abs_position = self.controllers.motors[motor_name].absolute_encoder.get_position()
+            rel_pos = self.controllers.motors[motor_name].get_encoder_counter_absolute(True)
+            self.controllers.motors[motor_name].go_to_speed_or_relative_position(self.ppr_to_mils(0))
+            time.sleep(2)
+            abs_position = self.controllers.motors[motor_name].absolute_encoder.get_position()
+
+    def ball_transfer(self, gamestation_a, gamestation_b):
+        # start timer_a and timer_b
+        # confirm bridges a and b are down
+        # turn gamestation_a carousel to receive ball
+        # turn gamestation_b carousel to receive ball
+        # if gamestation_a and gamestation_b receive balls before time runs out:
+            # rotate carousels to bridges
+            # confirm carousel positions
+            # lift bridges
+            # confirm balls are received in central carousel
+            # rotate 
+        time.sleep(1)
+    """
+
+    def run(self):
+        while True:
+            try:
+                topic, message, origin, destination = self.queue.get(True)
+                print(topic, message)
+            except Exception as e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
+
+main = Motor_Controllers()
+
+
+################################################
+# HARDWARE SEMANTICS (map of callable methods) #
+################################################
+
+motor_controller = {
+    "carousel1and2":{},
+    "carousel3and4":{},
+    "carousel5and6":{},
+}
+
+motor = {
+    "carousel_1":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_to_barter_tube_position":None,#(fruit_number)
+        "rotate_to_money_tube_position":None,#(fruit_number)
+        "rotate_to_center":None,#(fruit_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+    "carousel_2":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_to_barter_tube_position":None,#(fruit_number)
+        "rotate_to_money_tube_position":None,#(fruit_number)
+        "rotate_to_center":None,#(fruit_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{ #(send messages to carousel pi via controller)
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+    "carousel_3":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_to_barter_tube_position":None,#(fruit_number)
+        "rotate_to_money_tube_position":None,#(fruit_number)
+        "rotate_to_center":None,#(fruit_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{ #(send messages to carousel pi via controller)
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+    "carousel_4":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_to_barter_tube_position":None,#(fruit_number)
+        "rotate_to_money_tube_position":None,#(fruit_number)
+        "rotate_to_center":None,#(fruit_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{ #(send messages to carousel pi via controller)
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+    "carousel_5":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_to_barter_tube_position":None,#(fruit_number)
+        "rotate_to_money_tube_position":None,#(fruit_number)
+        "rotate_to_center":None,#(fruit_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{ #(send messages to carousel pi via controller)
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+    "carousel_6":{
+        "set_rel_encoder_position":None,#(relative position in encoder pulses)
+        "get_rel_encoder_position":None,
+        "get_abs_encoder_position":None,
+        "rotate_to_position_in_mills":None,#(relative position in mills)
+        "rotate_to_position_in_degrees":None,#(relative position in degrees)
+        "rotate_to_position_in_pulses":None,#(relative position in encoder pulses)
+        "rotate_fruit_number_to_gamestation_number":None,#(fruit_number,gamestation_number)
+        "eject_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "detect_ball":None,#(fruit_number) (send messages to carousel pi via controller)
+        "lights":{ #(send messages to carousel pi via controller)
+            "spinner":None,
+            "ripple":None,
+            "sparkle":None,
+            "fruit_1":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_2":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_3":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_4":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "fruit_5":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+            "dinero":{
+                "off":None,
+                "on":None,
+                "sparkle":None,
+                "blink":None
+            },
+        }
+    },
+}
+
+
+#############################################
+# ROUTINES (time, events, multiple systems) # 
+#############################################
+
+
+
+#############################################
 
 # Main handles network send/recv and can see all other classes directly
 class Main(threading.Thread):
@@ -203,5 +667,5 @@ class Main(threading.Thread):
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
-main = Main()
+#main = Main()
 
