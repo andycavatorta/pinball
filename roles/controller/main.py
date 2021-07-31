03323f6e216d6f88e7039034c5f46815b99b8b4a
Andy Cavatorta <hellyeah@media.mit.edu>
2021-07-31 18:39:05 -0500

    getting all pis running safely

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -41,6 +41,7 @@ class Safety_Enable(threading.Thread):
 
     def run(self):
         while True:
+            """
             time.sleep(setting_safety_enable_duration)
             GPIO.output(setting_safety_enable_gpio, GPIO.HIGH)
             time.sleep(setting_safety_enable_duration)
@@ -67,7 +68,6 @@ class Safety_Enable(threading.Thread):
                     GPIO.output(setting_safety_enable_gpio, GPIO.LOW)
                     self.enable_state_change_handler(self.enabled)
             self.hosts_alive = set()
-            """
 
 
 ##################################################
