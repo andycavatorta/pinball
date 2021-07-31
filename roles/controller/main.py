e42322c5837a69987b8ec2c481e75c3f3a5562e9
Andy Cavatorta <hellyeah@media.mit.edu>
2021-07-31 11:57:46 -0500

    working on deadman connection to nucleo

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -493,6 +493,7 @@ class Main(threading.Thread):
         while True:
             try:
                 topic, message, origin, destination = self.queue.get(True)
+                print(topic, message, origin, destination)
                 if topic==b"deadman":
                     self.safety_enable.add_to_queue(topic, message, origin, destination)
                 if topic==b"ready_state":
