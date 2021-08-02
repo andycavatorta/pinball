4dfaa656fed3a9dde8725b84abddce062afb448a
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-02 14:52:23 -0500

    making pinballmatrix start

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -57,4 +57,4 @@ class Main(threading.Thread):
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
-#main = Main()
+main = Main()
