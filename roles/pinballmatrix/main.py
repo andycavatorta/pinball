e196e5ca7d710afd3dff31c3ae1ca53271f7f20d
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-02 14:49:03 -0500

    making pinballmatrix start

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -241,7 +241,7 @@ class Motors(threading.Thread):
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
 
-motors = Motors() 
+
 
 
 ################################################
