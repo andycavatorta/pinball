5c013ec4fee84581779882b656bba3a76a6553b1
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-03 14:33:32 -0500

    fixing deadman timing

--- roles/controller/tests.py
+++ roles/controller/tests.py
@@ -19,10 +19,9 @@ class Displays():
                 time.sleep(.5)
                 self.tb.publish(topic="set_number",message=displayed_number-111,destination=destination)
                 time.sleep(.5)
-                role_module.main.tb.publish(topic="set_phrase",message=self.phrases[int(displayed_number/100)],destination=destination)
+                self.tb.publish(topic="set_phrase",message=self.phrases[int(displayed_number/100)],destination=destination)
             time.sleep(.5)
             displayed_number -= 111
-            self.tb.publish(topic="set_phrase",message=phrases[bi],destination=destination)
 
 """
 
