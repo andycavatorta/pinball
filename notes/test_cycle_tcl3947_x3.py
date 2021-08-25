d9033792ce82b8c6d88727aea9488ad32bc8c1c7
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-25 16:53:58 -0500

    for testing crabs

--- notes/test_cycle_tcl3947_x3.py
+++ notes/test_cycle_tcl3947_x3.py
@@ -6,7 +6,7 @@ import time
     
 spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI)
 latch = digitalio.DigitalInOut(board.D26)
-number_of_boards = 1
+number_of_boards = 3
 number_of_channels = number_of_boards * 24
 tlc5947 = adafruit_tlc5947.TLC5947(spi, latch, num_drivers=number_of_boards)
 
