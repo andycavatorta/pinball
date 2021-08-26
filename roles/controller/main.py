d9414f8147a08e51ea6e172eddf6ef7ba5853194
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-26 15:48:15 -0500

    typos

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -137,7 +137,7 @@ class Main(threading.Thread):
             self.exception_handler
         )
         #self.carousel_current_sensor = ina260_current_sensor.INA260()
-        self.safety_enable = Safety_Enable(self.tb)
+        self.safety_enable = Safety_Enable.Safety_Enable(self.tb)
 
         self.queue = queue.Queue()
 
