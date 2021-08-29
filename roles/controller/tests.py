f600e0624d2c1943de8abc2988950b37810b68b1
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-29 14:54:46 -0500

    =aesthetic adjustments

--- roles/controller/tests.py
+++ roles/controller/tests.py
@@ -130,7 +130,7 @@ class Displays():
                     self.tb.publish(topic="set_number",message=999,destination=destination)    
                 for destination in self.destinations:
                     rest = random.randint(0,3)
-                    if rest == 0:
+                    if rest != 0:
                         self.tb.publish(topic="play_score",message=pitches[random.randint(0,4)],destination=destination)
                     #if pitch_i != 0:
                     #    self.tb.publish(topic="play_score",message=pitches[0],destination=destination)
@@ -141,7 +141,7 @@ class Displays():
                     self.tb.publish(topic="set_phrase",message="juega",destination=destination)
                 for destination in self.destinations:
                     rest = random.randint(0,3)
-                    if rest == 0:
+                    if rest != 0:
                         self.tb.publish(topic="play_score",message=pitches[random.randint(0,4)],destination=destination)
                     #if pitch_i != 0:
                     #    self.tb.publish(topic="play_score",message=pitches[0],destination=destination)
