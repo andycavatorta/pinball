a46e6175434db2074ff510a6365927f59aa30c10
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-29 14:20:58 -0500

    =aesthetic adjustments

--- roles/controller/tests.py
+++ roles/controller/tests.py
@@ -106,7 +106,7 @@ class Displays():
 
 
     def wave(self):
-        interval = 0.4
+        interval = 0.3
         pitches = [
             "c_mezzo",
             "asharp_mezzo",
@@ -130,8 +130,8 @@ class Displays():
                     self.tb.publish(topic="set_number",message=999,destination=destination)    
                 for destination in self.destinations:
                     self.tb.publish(topic="play_score",message=pitches[pitch_i],destination=destination)
-                    #if pitch_i != 0:
-                    #    self.tb.publish(topic="play_score",message=pitches[0],destination=destination)
+                    if pitch_i != 0:
+                        self.tb.publish(topic="play_score",message=pitches[0],destination=destination)
                     time.sleep(interval/5)
                 #time.sleep(interval/2)
                 for destination in self.destinations:
