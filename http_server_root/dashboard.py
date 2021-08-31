f4e4205fc0e729ad6263ab68385d20c26bfb6a3c
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-31 16:47:53 -0500

    trying to excoriate all automatic system status messages

--- http_server_root/dashboard.py
+++ http_server_root/dashboard.py
@@ -71,31 +71,12 @@ class Message_Receiver(threading.Thread):
     def add_to_queue(self, topic, message):
         self.queue.put((topic, message))
 
-    def generate_system_status(self):
-        status_report = self.tb_ref.hardware_management.get_system_status()
-        status_report["tb_git_timestamp"] = self.tb_ref.tb_get_git_timestamp()
-        status_report["tb_scripts_version"] = self.tb_ref.tb_get_scripts_version()
-        status_report["app_git_timestamp"] = self.tb_ref.app_get_git_timestamp()
-        status_report["app_scripts_version("] = self.tb_ref.app_get_scripts_version()
-        status_report["hostname"] = self.tb_ref.get_hostname()
-        status_report["local_ip"] = self.tb_ref.get_local_ip()
-        status_report["global_ip"] = self.tb_ref.get_global_ip()
-        status_report["online_status"] = self.tb_ref.get_online_status()
-        status_report["connections"] = self.tb_ref.check_connections()
-        status_report["msg_timestamp"] = str(datetime.datetime.now().isoformat(' ', 'seconds'))
-        self.add_to_queue("status",status_report)
-
     def run(self):
         while True:
-            try:
-                topic, message = self.queue.get(block=True, timeout=self.tb_ref.settings.Dashboard.refresh_interval)
+                topic, message = self.queue.get(block=True)
                 print("topic, message",topic, message)
                 message_json = json.dumps([topic, message])
                 self.websocket.sendToClients(self.websocket,message_json)
-                # self.websocket.sendToClients(message_json)
-
-            except queue.Empty:
-                self.generate_system_status()
 
 def status_receiver(message):
     message_receiver.add_to_queue("status_event",message)
