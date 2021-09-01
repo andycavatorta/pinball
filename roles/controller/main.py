91fde92e185c5350fb5b9ace5c6eb94d1fceb52c
Andy Cavatorta <hellyeah@media.mit.edu>
2021-08-31 19:28:22 -0500

    integrating dash

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -289,7 +289,7 @@ class Main(threading.Thread):
                             origin, #hostname
                             "rpi", # device
                             "df",#data_name
-                            df
+                            df/1073741824
                         ]
                     )
                     self.send_to_dashboard(
