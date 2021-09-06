9b8f612a092cffe0f494652d42469d63c4bbd3e9
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-06 14:25:05 -0500

    refactor for modes

--- http_server_root/dashboard.py
+++ http_server_root/dashboard.py
@@ -77,7 +77,7 @@ class Message_Receiver(threading.Thread):
         while True:
             topic, message = self.queue.get(block=True)
             #print("topic, message",topic, message)
-            message_json = json.dumps([topic, message])
+            message_json = json.dumps([str(topic), str(message)])
             self.websocket.sendToClients(self.websocket,message_json)
             """
             try:
