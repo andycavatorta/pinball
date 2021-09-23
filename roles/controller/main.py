6eb18bb7c4007e9ee2dabdc4574db2af81553f11
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-23 09:34:48 -0500

    adding chimes

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -808,7 +808,7 @@ class Fake_Attraction_Mode(threading.Thread):
         ]
 
         self.carousel_start_end = [
-            [7,0,1,7],
+            [6,0,1,7],
             [5,-1,5,0],
             [3,-3,-2,4],
             [1,-5,-4,2],
