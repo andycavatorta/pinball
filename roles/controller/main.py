fddde2a5c9a42c74388d2eafa87fdc43f7b1f203
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-23 09:31:45 -0500

    adding chimes

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -858,9 +858,9 @@ class Fake_Attraction_Mode(threading.Thread):
             for station_ordinal in range(6):
                 self.tb.publish(topic="set_phrase",message="",destination=self.display_names[station_ordinal])
                 self.tb.publish(topic="all_off",message="",destination=self.display_names[station_ordinal])
-            time.sleep(5)
+            time.sleep(3)
             self.run_ball_motion_sim(1,0)
-            time.sleep(5)
+            time.sleep(3)
 fake_attraction_mode = Fake_Attraction_Mode()
 
 
