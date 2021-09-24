a4c3b5e0dee7fd75f764adcea9e780bc501fc3a5
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-24 01:06:38 -0500

    mpf events

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -1046,7 +1046,7 @@ class Fake_Attraction_Mode(threading.Thread):
             time.sleep(3)
             self.run_ball_motion_sim(4,2)
             """
-            time.sleep(10)
+            time.sleep(120)
 fake_attraction_mode = Fake_Attraction_Mode()
 
 
