318829095d0b3147870d0b352d931d841fc0b684
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-29 17:13:35 -0500

    modes

--- roles/display/main.py
+++ roles/display/main.py
@@ -287,11 +287,11 @@ class Main(threading.Thread):
     def request_system_tests(self):
         # computer details
         self.tb.publish(
-            topic="respond_computer_details", 
+            topic="response_computer_details", 
             message=self.request_computer_details()
         )
         self.tb.publish(
-            topic="respond_current_sensor_nominal",
+            topic="response_current_sensor_nominal",
             message=self.request_current_sensor_nominal()
         )
 
@@ -328,13 +328,13 @@ class Main(threading.Thread):
 
                 if topic == b'request_computer_details':
                     self.tb.publish(
-                        topic="respond_computer_details", 
+                        topic="response_computer_details", 
                         message=self.request_computer_details()
                     )
 
                 if topic == b'request_current_sensor_nominal':
                     self.tb.publish(
-                        topic="respond_current_sensor_nominal",
+                        topic="response_current_sensor_nominal",
                         message=self.request_current_sensor_nominal()
                     )
 
