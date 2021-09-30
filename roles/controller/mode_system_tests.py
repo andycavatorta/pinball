24d92f85fe40f7d06b7dad8ac1fec986dddca3ca
Andy Cavatorta <hellyeah@media.mit.edu>
2021-09-30 16:27:56 -0500

    preparing to test modes

--- roles/controller/mode_system_tests.py
+++ roles/controller/mode_system_tests.py
@@ -208,6 +208,7 @@ class Mode_System_Tests(threading.Thread):
                         origin = codecs.decode(origin, 'UTF-8')
                     if isinstance(destination, bytes):
                         destination = codecs.decode(destination, 'UTF-8')
+                    print("topic",topic)
                     print("getattr(self,topic)",getattr(self,topic))
                     getattr(self,topic)(
                             message, 
