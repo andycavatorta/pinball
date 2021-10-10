cc1552ed647fb83f36e6fcc104f8168d2ce41e6c
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-10 18:21:10 -0500

    working on encoder calibration

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -226,7 +226,7 @@ class Main(threading.Thread):
         if message: # if power on
             self.high_power_init = True
             self.create_controllers_and_motors()
-            #self.get_absolute_positions()
+            self.get_absolute_positions()
             #self.sync_relative_encoders_to_absolute_encoders()
             #for motor_name in self.motor_names:
             #    self.controllers.motors[motor_name].rotate_to_position = Rotate_to_Position(self.controllers.motors[motor_name], self.add_to_queue)
@@ -234,9 +234,9 @@ class Main(threading.Thread):
             #for motor_name in self.motor_names:
             #    time.sleep(0.1)
             #    print("sDC values",motor_name,self.controllers.motors[motor_name].get_encoder_counter_absolute(True))
-            self.absolute_encoders_presences = [True,True,True,True,True,True]
-            self.absolute_encoders_positions = [0,0,0,0,0,0]
-            self.absolute_encoders_zeroed = [True,True,True,True,True,True]
+            #self.absolute_encoders_presences = [True,True,True,True,True,True]
+            #self.absolute_encoders_positions = [0,0,0,0,0,0]
+            #self.absolute_encoders_zeroed = [True,True,True,True,True,True]
         else: # if power off
             self.high_power_init = False
             self.absolute_encoders_presences = [False,False,False,False,False,False]
