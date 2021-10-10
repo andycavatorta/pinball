41a2183efea64e90b6809fcd011ad5fd839fd210
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-09 22:03:21 -0500

    incrementally testing mixed use of encoders

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -179,15 +179,19 @@ class Main(threading.Thread):
         if self.high_power_init == True:
             # create SPI interfaces for AMT203
             time.sleep(3)
+            print(">>>>> Main get_absolute_positions 0")
             self.absolute_encoders = AMT203(gpios_for_chip_select=[12,13,17,18,5,16], speed_hz = 5859375)
             time.sleep(3)
             # verify that encoders are present
+            print(">>>>> Main get_absolute_positions 1")
             self.absolute_encoders_presences = self.absolute_encoders.get_presences()
             time.sleep(3)
             # read absolute positions
+            print(">>>>> Main get_absolute_positions 2")
             self.absolute_encoders_positions = self.absolute_encoders.get_positions()
             time.sleep(3)
             # stop SPI interfaces - spidev.close()
+            print(">>>>> Main get_absolute_positions 3")
             self.absolute_encoders.close()
             time.sleep(1)
 
