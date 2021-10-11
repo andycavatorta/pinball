1686749cf85e7ca63df5ce8c20d97572bbfa2710
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-10 19:13:39 -0500

    working on motion calibration

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -229,12 +229,12 @@ class Main(threading.Thread):
             self.create_controllers_and_motors()
             self.get_absolute_positions()
             self.sync_relative_encoders_to_absolute_encoders()
-            #for motor_name in self.motor_names:
-            #    self.controllers.motors[motor_name].rotate_to_position = Rotate_to_Position(self.controllers.motors[motor_name], self.add_to_queue)
-            #print("AMT values:",self.absolute_encoders_positions)
             for motor_name in self.motor_names:
-                time.sleep(0.1)
-                print("sDC values",motor_name,self.controllers.motors[motor_name].get_encoder_counter_absolute(True))
+                self.controllers.motors[motor_name].rotate_to_position = Rotate_to_Position(self.controllers.motors[motor_name], self.add_to_queue)
+            #print("AMT values:",self.absolute_encoders_positions)
+            #for motor_name in self.motor_names:
+            #    time.sleep(0.1)
+            #    print("SDC values",motor_name,self.controllers.motors[motor_name].get_encoder_counter_absolute(True))
             #self.absolute_encoders_presences = [True,True,True,True,True,True]
             #self.absolute_encoders_positions = [0,0,0,0,0,0]
             #self.absolute_encoders_zeroed = [True,True,True,True,True,True]
