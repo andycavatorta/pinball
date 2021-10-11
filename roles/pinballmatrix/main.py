14ba9bcfbe1468e26bafe7e2446097efd1b65ed7
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-10 19:40:38 -0500

    working on motion calibration

--- roles/pinballmatrix/main.py
+++ roles/pinballmatrix/main.py
@@ -227,7 +227,7 @@ class Main(threading.Thread):
         if message: # if power on
             self.high_power_init = True
             self.create_controllers_and_motors()
-            #self.get_absolute_positions()
+            self.get_absolute_positions()
             #self.sync_relative_encoders_to_absolute_encoders()
             #for motor_name in self.motor_names:
             #    self.controllers.motors[motor_name].rotate_to_position = Rotate_to_Position(self.controllers.motors[motor_name], self.add_to_queue)
