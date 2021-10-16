99cb98baebbea5a687ec77bde009c8787792cfae
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-15 19:18:30 -0500

    more power

--- roles/carousel/solenoids.py
+++ roles/carousel/solenoids.py
@@ -7,7 +7,7 @@ class Solenoids(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.queue = queue.Queue()
-        self.eject_pulse_time = 0.05
+        self.eject_pulse_time = 0.25
         GPIO.setmode(GPIO.BCM)
         self.solenoid_pins = [1,2,3,4,5] # todo: update later
         for solenoid_pin in self.solenoid_pins:
