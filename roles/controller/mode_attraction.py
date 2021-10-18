a4168f7160cfc37d21ef68e7f50a6564e0326cfd
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-18 11:25:56 -0500

    fake demo for Governor

--- roles/controller/mode_attraction.py
+++ roles/controller/mode_attraction.py
@@ -293,9 +293,9 @@ class Animation(threading.Thread):
                             for hostname in self.display_hostnames:
                                 if random.randrange(0,3) == 0:
                                     self.hosts.hostnames[hostname].request_score(self.mezzo_chimes[random.randrange(0,5)])
-                    if self.animation_frame_counter % 50 == 0:                              
+                    if self.animation_frame_counter % 1550 == 0:                              
                         for motor_name in self.motor_names:
-                            print("carousel_position", carousel_position)
+                            print("carousel_position", animation_frame_counter, carousel_position)
                             if carousel_position == "left":
                                 if motor_name == "carousel_center":
                                     self.hosts.pinballmatrix.cmd_rotate_carousel_to_target("carousel_center","sandia","sandia")
