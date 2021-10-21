7ddad4d5640610a4e6768b08b8a28c6097f37e2b
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-21 17:07:34 -0500

    carousel motions

--- roles/controller/mode_attraction.py
+++ roles/controller/mode_attraction.py
@@ -293,7 +293,6 @@ class Animation(threading.Thread):
                             for hostname in self.display_hostnames:
                                 if random.randrange(0,3) == 0:
                                     self.hosts.hostnames[hostname].request_score(self.mezzo_chimes[random.randrange(0,5)])
-                    """
                     if self.animation_frame_counter % 50 == 0:
                         for motor_name in self.motor_names:
                             print("carousel_position", self.animation_frame_counter, carousel_position)
@@ -311,7 +310,6 @@ class Animation(threading.Thread):
                             carousel_position = "left"
                         else:
                             carousel_position = "right"
-                    """
 
                     self.animation_frame_counter += 1
                 else:
