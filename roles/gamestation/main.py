bae39c3136348a6f79b6a0d611ffb4477d476511
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-28 04:57:17 -0500

    refactor

--- roles/gamestation/main.py
+++ roles/gamestation/main.py
@@ -574,22 +574,22 @@ class Main(threading.Thread):
                 if topic == 'event_rollover_inner_left':
                     self.tb.publish(
                         topic="event_roll_inner_left",
-                        message=True if message=="closed" else False
+                        message=True
                     )
                 if topic == 'event_rollover_inner_right':
                     self.tb.publish(
                         topic="event_roll_inner_right",
-                        message=True if message=="closed" else False
+                        message=True
                     )
                 if topic == 'event_rollover_outer_left':
                     self.tb.publish(
                         topic="event_roll_outer_left",
-                        message=True if message=="closed" else False
+                        message=True
                     )
                 if topic == 'event_rollover_outer_right':
                     self.tb.publish(
                         topic="event_roll_outer_right",
-                        message=True if message=="closed" else False
+                        message=True
                     )
                 if topic == 'event_slingshot_left':
                     self.tb.publish(
