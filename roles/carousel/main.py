a720540d5bdb33b5583889fab5b885cb55ff49b4
Andy Cavatorta <hellyeah@media.mit.edu>
2021-12-18 22:30:26 -0600

    finishing hosts functions

--- roles/carousel/main.py
+++ roles/carousel/main.py
@@ -103,8 +103,7 @@ class Inductive_Sensors(threading.Thread):
                 self.queue.get(True,0.1)
                 states = {}
                 for sensor in self.sensors:
-                    states[sensor.name] = sensor.get_state()[1]
-                    print("states",states)
+                    states[sensor.name] = True if sensor.get_state()[1]==0 else False
                 self.tb.publish("response_carousel_detect_balls",states)
             except queue.Empty:
                 for sensor in self.sensors:
