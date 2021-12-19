f582c8a3924e3e81d7bd3d6932f4b6e40e30092d
Andy Cavatorta <hellyeah@media.mit.edu>
2021-12-18 23:40:31 -0600

    encoder problem

--- roles/controller/mode_inventory.py
+++ roles/controller/mode_inventory.py
@@ -206,9 +206,10 @@ class Mode_Inventory(threading.Thread):
         # eject
         for i in range(retries):
             self.hosts.hostnames[carousel_name].request_eject_ball(fruit_name)
-            time.sleep(0.25)
+            time.sleep(0.4)
             carousel_balls_detected = self.hosts.hostnames[carousel_name].get_carousel_ball_detected()
             if carousel_balls_detected[fruit_name] == False:
+                print(">>>", self.hosts.hostnames[pinball_name].get_count_tube_sensor_events_right(2))
                 if tube_left_right == "left":
                     if self.hosts.hostnames[pinball_name].get_count_tube_sensor_events_left(2) > 0:
                         return [True,""]
