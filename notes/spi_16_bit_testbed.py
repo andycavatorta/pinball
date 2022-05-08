434d068646feb3f6ffed1b043c56b67cb49ef8a6
Andy Cavatorta <hellyeah@media.mit.edu>
2022-05-08 16:38:30 -0400

    hardware test

---
+++ notes/spi_16_bit_testbed.py
@@ -0,0 +1,57 @@
+import queue
+import spidev 
+import threading
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+class Square_Wave_Generator(threading.Thread):
+    def __init__(self, gpio_number, frequency_in_hz):
+        threading.Thread.__init__(self)
+        self.gpio_number = gpio_number
+        self.period = 1.0/frequency_in_hz
+        GPIO.setup(self.gpio_number, GPIO.OUT)
+
+    def run(self):
+        while True:
+          GPIO.output(self.gpio_number, GPIO.LOW )
+          time.sleep(self.period)
+          GPIO.output(self.gpio_number, GPIO.HIGH )
+          time.sleep(self.period)
+
+class SPI_16_Bit(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = queue.Queue()
+        """
+        self.spi = spidev.SpiDev()
+        self.spi.open(self.bus, self.deviceId)
+        self.open = True
+        self.spi.mode = 0b00
+        self.spi.no_cs = True
+        self.spi.max_speed_hz = speed_hz
+        """
+
+    def add_to_queue(self, gain_int):
+        self.queue.put(gain_int)
+
+    def run(self):
+        while True:
+            gain_int = self.queue.get(True)
+            gain_16_bits 65536.0 * float(gain_int) /100.0
+            print(gain_16_bits)
+            #self.spi.writebytes( values )
+
+class CLI(threading.Thread):
+    def __init__(self, ):
+        threading.Thread.__init__(self)
+        self.square_wave_generator = Square_Wave_Generator(26, 1000)
+        self.square_wave_generator.start()
+        self.spi_16_bit = SPI_16_Bit()
+        self.spi_16_bit.start()
+
+    def run(self):
+        while True:
+            input_str = raw_input("please enter gain as a number between 0-100")
+            input_float = float(input_str)
+            self.spi_16_bit.add_to_queue(input_float)
