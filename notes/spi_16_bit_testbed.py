74965482df715c21fafda6dee22759d9ed68aea0
Andy Cavatorta <hellyeah@media.mit.edu>
2022-05-08 18:11:40 -0400

    hardware test

--- notes/spi_16_bit_testbed.py
+++ notes/spi_16_bit_testbed.py
@@ -23,15 +23,19 @@ class SPI_16_Bit(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.queue = queue.Queue()
-        GPIO.setup(19, GPIO.OUT)
-        GPIO.output(19, GPIO.HIGH )
+        self.zcen_gpio = 19
+        self.cs_gpio = 13
+        GPIO.setup(self.zcen_gpio, GPIO.OUT)
+        GPIO.output(self.zcen_gpio, GPIO.HIGH )
+        GPIO.setup(self.cs_gpio, GPIO.OUT)
+        GPIO.setup(self.cs_gpio, GPIO.HIGH)
 
         self.spi = spidev.SpiDev()
-        self.spi.open(1, 0)
+        self.spi.open(0, 0)
         self.open = True
         self.spi.mode = 0b00
-        self.spi.bits_per_word = 16
-        #self.spi.no_cs = True
+        #self.spi.bits_per_word = 16
+        self.spi.no_cs = True
         self.spi.max_speed_hz = 500000
 
     def add_to_queue(self, gain_int):
@@ -41,9 +45,14 @@ class SPI_16_Bit(threading.Thread):
         while True:
             gain_int = self.queue.get(True)
             gain_16_bits = int(65536.0 * float(gain_int) /100.0)
+            high_byte = gain_16_bits >> 8
+            low_byte = gain_16_bits & 0x00FF
             print("")
-            print(gain_16_bits)
-            self.spi.writebytes(gain_16_bits)
+            print(gain_16_bits, high_byte, low_byte)
+            GPIO.setup(self.cs_gpio, GPIO.LOW)
+            self.spi.writebytes(high_byte)
+            self.spi.writebytes(low_byte)
+            GPIO.setup(self.cs_gpio, GPIO.HIGH)
 
 class CLI(threading.Thread):
     def __init__(self, ):
