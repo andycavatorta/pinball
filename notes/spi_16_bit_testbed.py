ae9c73062a3b904ab145a118faa7e5b3057f8d09
Andy Cavatorta <hellyeah@media.mit.edu>
2022-05-08 21:54:36 -0400

    hardware test

--- notes/spi_16_bit_testbed.py
+++ notes/spi_16_bit_testbed.py
@@ -57,7 +57,7 @@ class SPI_16_Bit(threading.Thread):
             print(gain_16_bits, high_byte, low_byte)
             GPIO.output(self.cs_gpio, GPIO.LOW)
             print(1)
-            self.wpi.wiringPiSPIDataRW(0, chr(128) + chr(128)) # set volume to zero as test of comms
+            self.wpi.wiringPiSPIDataRW(0, chr(128) + chr(128), 16) # set volume to zero as test of comms
             #self.spi.writebytes([65536])
             print(2)
             GPIO.output(self.cs_gpio, GPIO.HIGH)
